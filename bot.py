3340d377b830e8c4d3702e83f17701fdcca8fec4
Schaffer <mr.giancarloschaffer@gmail.com>
2022-08-12 18:33:47 -0300

    testando pyrogram

---
+++ bot.py
@@ -0,0 +1,22 @@
+from decouple import config
+from pyrogram import Client, filters
+from datetime import datetime
+
+TOKEN_TELEGRAM = config("TOKEN_TELEGRAM")
+API_HASH = config("API_HASH")
+API_ID = config("API_ID")
+
+app = Client('a_semana_bot', api_hash=API_HASH,
+             api_id=API_ID, bot_token=TOKEN_TELEGRAM)
+
+
+@app.on_message(filters.command('encerrada'))
+async def hello(client, message):
+    print(message.date)
+    d = datetime.fromisoformat(str(message.date))
+    print(d.weekday())
+    print(d.strftime('%H'))
+    await message.reply("Hello from Pyrogram!")
+
+
+app.run()
